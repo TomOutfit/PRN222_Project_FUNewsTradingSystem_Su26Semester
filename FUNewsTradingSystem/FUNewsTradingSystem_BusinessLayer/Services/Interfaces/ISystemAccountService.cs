d255b63bba9365d152b71f1d238b4aa89121e64e
Nguyễn Bình An <167419245+TomOutfit@users.noreply.github.com>
2026-05-25 17:51:38 +0700

    Feat: Project Setup - Business Layer

---
+++ FUNewsTradingSystem/FUNewsTradingSystem_BusinessLayer/Services/Interfaces/ISystemAccountService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FUNewsTradingSystem_BusinessLayer.Services.Interfaces
+{
+    internal interface SystemAccountService
+    {
+    }
+}
