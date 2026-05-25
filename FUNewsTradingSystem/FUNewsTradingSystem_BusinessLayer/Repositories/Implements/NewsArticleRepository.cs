f4d796d83d8c21228ebc55f22db5d38c225a1b59
Nguyễn Bình An <167419245+TomOutfit@users.noreply.github.com>
2026-05-25 17:51:38 +0700

    Feat: Project Setup - Business Layer

---
+++ FUNewsTradingSystem/FUNewsTradingSystem_BusinessLayer/Repositories/Implements/NewsArticleRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FUNewsTradingSystem_BusinessLayer.Repositories.Interfaces
+{
+    public class NewsArticleRepository
+    {
+    }
+}
