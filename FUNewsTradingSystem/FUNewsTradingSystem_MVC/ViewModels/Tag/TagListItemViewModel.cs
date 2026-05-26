e0f5b3d630c0dcd3ff84ea03df93be915bd290aa
teng122 <donghoangnguyen123@gmail.com>
2026-05-26 15:38:20 +0700

    Fix: ViewModels missing "Id"

--- FUNewsTradingSystem/FUNewsTradingSystem_MVC/ViewModels/Tag/TagListItemViewModel.cs
+++ FUNewsTradingSystem/FUNewsTradingSystem_MVC/ViewModels/Tag/TagListItemViewModel.cs
@@ -2,5 +2,7 @@ namespace FUNewsTradingSystem_MVC.ViewModels.Tag;
 
 public class TagListItemViewModel
 {
-
+    public int TagId { get; set; }
+    public string TagName { get; set; } = string.Empty;
+    public string? Note { get; set; }
 }
