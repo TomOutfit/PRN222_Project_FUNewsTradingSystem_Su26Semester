3b4f253afefaca5de4f83644ce90889000e15153
cuonglkhe191111 <cuongttgl1975@gmail.com>
2026-06-04 10:05:51 +0700

    feat(database): create ServiceResult class and generate SeedData migration for categories and tags

--- FUNewsTradingSystem/DataAccessLayer/Migrations/20260604030324_SeedData.Designer.cs
+++ FUNewsTradingSystem/DataAccessLayer/Migrations/20260604030324_SeedData.Designer.cs
@@ -4,6 +4,7 @@ using FUNewsTradingSystem_DataAccessLayer.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FUNewsTradingSystem_DataAccessLayer.Migrations
 {
     [DbContext(typeof(FUNewsManagementContext))]
-    partial class FUNewsManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20260604030324_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
