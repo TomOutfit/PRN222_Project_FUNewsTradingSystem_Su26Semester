f8026ec7d8602abfe1349c484eeaa1c6a2a6bbb0
cuonglkhe191111 <cuongttgl1975@gmail.com>
2026-06-04 10:47:15 +0700

    feat(admin): implement account management CRUD with viewmodels and ajax modals

--- FUNewsTradingSystem/MVC/obj/Debug/net10.0/MVC.AssemblyInfo.cs
+++ FUNewsTradingSystem/MVC/obj/Debug/net10.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9600d91a2a5f7b5374805ea8d26eaf1771acec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8105437a31a32fd190bd407f4a3791649f4b55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
