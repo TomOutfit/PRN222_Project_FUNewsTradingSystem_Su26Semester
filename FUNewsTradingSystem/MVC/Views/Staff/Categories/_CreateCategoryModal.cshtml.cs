b8432f8e7129037837121b1535d190bd2f97610f
teng122 <donghoangnguyen123@gmail.com>
2026-06-05 16:32:58 +0700

    feat: implement category management

---
+++ FUNewsTradingSystem/MVC/Views/Staff/Categories/_CreateCategoryModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVC.Views.Staff.Categories
+{
+    public class _CreateCategoryModalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
