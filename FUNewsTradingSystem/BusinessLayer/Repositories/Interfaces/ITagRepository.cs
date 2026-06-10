6d35c23707ee125dab853c54ff2a74c336d32d01
teng122 <donghoangnguyen123@gmail.com>
2026-06-10 11:06:10 +0700

    feat: implement tag management page

--- FUNewsTradingSystem/BusinessLayer/Repositories/Interfaces/ITagRepository.cs
+++ FUNewsTradingSystem/BusinessLayer/Repositories/Interfaces/ITagRepository.cs
@@ -5,12 +5,10 @@ namespace FUNewsTradingSystem_BusinessLayer.Repositories.Interfaces
     public interface ITagRepository
     {
         Task<List<Tag>> GetAllAsync();
-        Task<List<Tag>> GetAllForDropdownAsync();
         Task<Tag?> GetByIdAsync(int id);
-        Task<bool> TagNameExistsAsync(string name, int? excludeId = null);
-        Task<bool> IsReferencedByAnyArticleAsync(int tagId);
-        Task<Tag> CreateAsync(Tag tag);
+        Task AddAsync(Tag tag);
         Task UpdateAsync(Tag tag);
         Task DeleteAsync(int id);
+        Task<bool> ExistsByNameAsync(string tagName);
     }
 }
---
+++ FUNewsTradingSystem/MVC/ViewModels/Tag/EditTagViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FUNewsTradingSystem_MVC.ViewModels.Tag;
+
+public class EditTagViewModel
+{
+    [Required]
+    public int TagID { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string TagName { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string? Note { get; set; }
+}
