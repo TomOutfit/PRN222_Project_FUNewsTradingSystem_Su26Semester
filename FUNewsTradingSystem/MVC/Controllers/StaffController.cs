c19217a64067276f2ffec9aa9f408e70e2cfe0ba
phunvqe180136 <phunvqe180136@fpt.edu.vn>
2026-06-10 12:18:24 +0700

    feat: Add GET Method to Staff Controller

--- FUNewsTradingSystem/MVC/Controllers/StaffController.cs
+++ FUNewsTradingSystem/MVC/Controllers/StaffController.cs
@@ -43,7 +43,7 @@ public class StaffController : Controller
             ViewData["ReportCount"] = 0;
         }
 
-        return View();
+        return View("Dashboard/Index");
     }
 
     /// <summary>
